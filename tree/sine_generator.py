83a85574095479049ef4ca192627f186635a9005
Matthias <mail@matthiasbock.net>
2016-09-22 17:03:30 +0200

    quick and dirty sine table generator

---
+++ tree/sine_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# Quick and dirty sine value table generator
+#
+
+from math import sin
+
+f = open("sine_table.c", "w")
+
+f.write("const int sine[360] = {\n")
+
+for i in range(360):
+    f.write( "\t"+str(int(256*sin(i * 3.1415 / 180))) )
+    if i < 359:
+        f.write( ",\n" )
+    
+f.write("\n};\n")
