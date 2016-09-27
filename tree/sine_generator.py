5cb99d5b788fdbedc3829eab3d065b97f71b0361
Matthias <mail@matthiasbock.net>
2016-09-27 16:00:28 +0200

    sine generator produces values between 0 and 255

--- tree/sine_generator.py
+++ tree/sine_generator.py
@@ -10,7 +10,8 @@ f = open("sine_table.c", "w")
 f.write("const int sine[360] = {\n")
 
 for i in range(360):
-    f.write( "\t"+str(int(256*sin(i * 3.1415 / 180))) )
+    #f.write( "\t"+str(int(256*sin(i * 3.1415 / 180))) )
+    f.write( "\t"+str(int(128 + (127.5*sin(i * 3.1415 / 180)))) )
     if i < 359:
         f.write( ",\n" )
     
