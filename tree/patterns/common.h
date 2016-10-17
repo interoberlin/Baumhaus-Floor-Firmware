2f48c5cce1d93a5c61c56f732759b3b1de98a2d1
Matthias <mail@matthiasbock.net>
2016-10-17 13:58:28 +0200

    added patterns/common.h

---
+++ tree/patterns/common.h
@@ -0,0 +1,15 @@
+#ifndef PATTERN_COMMON_H
+#define PATTERN_COMMON_H
+
+/*
+ * 2,40m LED strips, 60 LEDs/m = per strip 144 LEDs in total
+ */
+#define LED_COUNT 144
+
+/*
+ * Pins to the data inputs of the addressable LED strips
+ */
+#define PIN_LEDSTRIP1 25
+#define PIN_LEDSTRIP2 26
+
+#endif
